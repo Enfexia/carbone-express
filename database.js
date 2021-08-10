ebdd3423b0324ec2aef85fd3a5541d99b697122a
Cem Cevik <cemcevik@uni-bremen.de>
2021-08-10 23:24:54 +0200

    fixed the database connection

--- database.js
+++ database.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-  mongoose.connect("mongodb://db:27017/forge", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  mongoose.connect(
+    process.env.MONGODB_URI || `mongodb://localhost:27017/forge`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
 
   mongoose.connection.on("open", () => {
     console.log("MongoDB: Connected");
